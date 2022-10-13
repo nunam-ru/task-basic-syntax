976212b8f8552bc483927486feaf7b3a530ccb4b
Dima Kryshkovec <nunam@internet.ru>
2022-10-13 23:31:31 +0300

    task complete

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,14 @@
 export function romanToInteger(str) {
     let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
+    const dict = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+    for (let i = 0; i < str.length; i++) {
+        const curr = dict[str[i]];
+        const next = dict[str[i + 1]];
+        if (curr < next) {
+            result -= curr;
+        } else {
+            result += curr;
+        }
+    }
     return result;
 }
